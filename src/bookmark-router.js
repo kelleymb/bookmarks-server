c47e4b538515b71ef72f02e79b930c816abf380d
Kelleymb <kelleybuccieri@gmail.com>
2020-06-07 21:01:01 -0400

    updates to route /bookmarks

--- src/bookmark-router.js
+++ src/bookmark-router.js
@@ -1,63 +1,45 @@
 const express = require('express')
 const { v4: uuid } = require('uuid')
+const { isWebUri } = require('valid-url')
 const logger = require('../logger')
-const { bookmarks } = require('./store')
+const store = require('../store')
+
 const bookmarkRouter = express.Router()
 const bodyParser = express.json()
 
 bookmarkRouter
     .route('/bookmarks')
     .get((req, res) => {
-        res
-            .json(bookmarks);
+        res.json(bookmarks);
     })
     .post(bodyParser, (req, res) => {
-        const { header, id } = req.body;
-
-        if (!header) {
-            logger.error(`Header is required`);
-            return res
-              .status(400)
-              .send('Invalid data.');
-        }
-
-        //validate bookmark id
-        if(id.length > 0) {
-            let valid = true;
-            id.forEach(bid => {
-                const bookmark = bookmarks.find(b => b.id == bid);
-                if(!bookmark) {
-                    logger.error(`Bookmark with id ${id} not found.`);
-                    valid = false;
-                }
-            });
-
-            if(!valid) {
-                return res
-                    .status(400)
-                    .send('Invalid data.')
+        for (const field of ['title', 'url', 'rating']) {
+            if (!req.body[field]) {
+              logger.error(`${field} is required`)
+              return res.status(400).send(`'${field}' is required`)
             }
-        }
-
-        // generate an id/get an id
-        const id = uuid();
-
-        const newBookmark = {
-            id,
-            title,
-            url,
-            rating,
-            desc
-        }
-
-        bookmarks.push(newBookmark);
-
-        logger.info(`A bookmark with ${id} was created`);
-
-        res
+          }
+          const { title, url, description, rating } = req.body
+      
+          if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
+            logger.error(`Invalid rating '${rating}' supplied`)
+            return res.status(400).send(`'rating' must be a number between 0 and 5`)
+          }
+      
+          if (!isWebUri(url)) {
+            logger.error(`Invalid url '${url}' supplied`)
+            return res.status(400).send(`'url' must be a valid URL`)
+          }
+      
+          const bookmark = { id: uuid(), title, url, description, rating }
+      
+          store.bookmarks.push(bookmark)
+      
+          logger.info(`Bookmark with id ${bookmark.id} created`)
+          res
             .status(201)
-            .location(`http://localhost:8000/bookmarks/${id}`)
-            .json({id})
+            .location(`http://localhost:8000/bookmarks/${bookmark.id}`)
+            .json(bookmark)
     })
 
 bookmarkRouter
@@ -65,7 +47,7 @@ bookmarkRouter
     .get((req, res) => {
         const { id } = req.params;
         
-        const bookmark = bookmarks.find(bm => bm.id == id);
+        const bookmark = store.bookmarks.find(bm => bm.id == id);
 
         //make sure we find a bookmark
         if(!bookmark) {
--- src/store.js
+++ src/store.js
@@ -1,23 +1,27 @@
+const { v4: uuid } = require('uuid')
+
 const bookmarks = [
     {
-      id: 0,
+      id: uuid(),
       title: 'Google',
       url: 'http://www.google.com',
       rating: '3',
       desc: 'Internet-related services and products.'
     },
     {
-      id: 1,
+      id: uuid(),
       title: 'Thinkful',
       url: 'http://www.thinkful.com',
       rating: '5',
       desc: '1-on-1 learning to accelerate your way to a new high-growth tech career!'
     },
     {
-      id: 2,
+      id: uuid(),
       title: 'Github',
       url: 'http://www.github.com',
       rating: '4',
       desc: 'brings together the world\'s largest community of developers.'
     }
-  ];
+];
+
+module.exports = { bookmarks }
